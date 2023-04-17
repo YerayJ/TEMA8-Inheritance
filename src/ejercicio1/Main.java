67fe3567b8a39479d4200432f064af61ffa6d93b
YerayJ <you@example.com>
2023-04-17 20:22:55 +0200

    Ejercicio 1 Primer boletín

--- src/ejercicio1/Main.java
+++ src/ejercicio1/Main.java
@@ -1,4 +1,15 @@
 package ejercicio1;
 
 public class Main {
+    static Hora hora = new Hora(12,59);
+    static HoraExacta horaExacta = new HoraExacta(20,59,59);
+
+    public static void main(String[] args) {
+        hora.inc();
+        System.out.println(hora.toString());
+
+        horaExacta.inc();
+        System.out.println(horaExacta.toString());
+    }
+
 }
