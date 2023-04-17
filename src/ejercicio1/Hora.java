c10b9eb3795e7a48d4e833bec4293b8d190b306a
YJ <yjimenez@iesnervion.es>
2023-04-17 19:02:02 +0200

    Initial commit

---
+++ src/ejercicio1/Hora.java
@@ -0,0 +1,77 @@
+package ejercicio1;
+
+/**
+ * Clase Hora
+ */
+public class Hora {
+    /**
+     * Atributo hora
+     */
+    private int hora;
+    /**
+     * Atributo minuto
+     */
+    private int minuto;
+
+    /**
+     * Constructor con parámetros
+     *
+     * @param hora   Contiene la hora
+     * @param minuto Contiene los minutos
+     */
+    public Hora(int hora, int minuto) {
+        if (hora > 23 || hora < 0) { // Comprueba si la hora está fuera del rango
+            hora = 0; // Si es asi la hora se le da valor de 0
+        } else if (minuto > 59 || minuto < 0) { // Comprueba si los minutos están fuera del rango
+            minuto = 0; // Si es asi los minutos se le da valor de 0
+        }
+        // Se le asigna los valores una vez comprobado
+        this.hora = hora;
+        this.minuto = minuto;
+    }
+
+    /**
+     * Método para incrementar en uno los minutos
+     */
+    public void inc() {
+        this.minuto++; // Incrementa los minutos en uno
+        if (this.minuto > 59) { // Comprueba si los minutos han pasado a una hora
+            this.minuto = 0; // Se asigna 0 a los minutos
+            this.hora++; // Se le suma uno a la hora
+        }
+    }
+
+    /**
+     * Setter
+     * @param valor El valor a asignar
+     * @return Devuelve el estado de la operación
+     */
+    boolean setMinutos(int valor) {
+        boolean estado = false; // Atributo de tipo boolean
+        if (valor >= 0 && valor < 60) { // Comprueba que los minutos sean correctos
+            this.minuto = valor; // Asigna valor a minuto
+        }
+        return estado; // Devuelve el estado de la operación
+    }
+
+    /**
+     * Setter
+     * @param valor El valor a asignar
+     * @return Devuelve el estado de la operación
+     */
+    boolean setHora(int valor) {
+        boolean estado = false; // Atributo de tipo boolean
+        if (valor >= 0 && valor < 24) { // Comprueba que la hora sea correcta
+            this.minuto = valor; // Asigna valor a la hora
+        }
+        return estado; // Devuelve el estado de la operación
+    }
+
+    /**
+     * Método para convertir en String
+     * @return Devuelve una string
+     */
+    public String toString() {
+        return (hora <= 9 ? "0" + hora : hora) + ":" + (hora <= 9 ? "0" + minuto : minuto);
+    }
+}
---
+++ src/ejercicio1/HoraExacta.java
@@ -0,0 +1,56 @@
+package ejercicio1;
+
+/**
+ * Clase hora exacta
+ */
+public class HoraExacta extends Hora {
+    /**
+     * Atributo que contiene los segundos
+     */
+    private int segundo;
+
+    /**
+     * Constructor con parámetros
+     *
+     * @param hora    Contiene la hora
+     * @param minuto  Contiene los minutos
+     * @param segundo Contiene los segundos
+     */
+    public HoraExacta(int hora, int minuto, int segundo) {
+        super(hora, minuto); // Llama al constructor de la clase padre
+        if (segundo < 0 || segundo > 59) { // Comprueba que los segundos están correctos
+            segundo = 0; // Asigna los segundos
+        }
+        this.segundo = segundo; // Se asignan los segundos
+    }
+
+    public boolean setSegundo(int valor) {
+        boolean estado = false;
+        if (segundo < 0 || segundo > 59) { // Comprueba que los segundos están correctos
+            valor = 0; // Asigna los segundos
+        }
+        this.segundo = valor; // Se asignan los segundos
+        return estado;
+    }
+
+    /**
+     * Método que incrementa en uno los segundos
+     */
+    @Override
+    public void inc() {
+        this.segundo++; // Incrementa los segundos en uno
+        if (segundo > 59) { // Si los segundos se sobrepasan
+            this.segundo = 0;  // Le da valor 0 a segundos
+            super.inc(); // Llama a la función incremento de la clase padre
+        }
+    }
+
+    /**
+     * Método que devuelve una String con todos los atributos
+     * @return Devuelve una String
+     */
+    @Override
+    public String toString() {
+        return super.toString() + ":" + (segundo <= 9 ? "0" + segundo : segundo);
+    }
+}
