e9ddf695d5c5763c366a84dec274b6d422966de9
YerayJ <you@example.com>
2023-05-08 17:53:17 +0200

    Ejercicio 4 Interfaces terminado

--- src/ejercicios/interfaces/ejercicio3/Main.java
+++ src/ejercicios/interfaces/ejercicio3/Main.java
@@ -3,21 +3,21 @@ package ejercicios.interfaces.ejercicio3;
 public class Main {
     public static void main(String[] args) {
         // Creación de arrays
-        Polideportivo polideportivos[] = {
+        InstalacionDeportiva polideportivos[] = {
                 new Polideportivo(30, "Hytasa", 2),
                 new Polideportivo(100, "UPO", 5)
         };
 
-        EdificioOficinas edificioOficinas[] = {
+        Edificio edificioOficinas[] = {
                 new EdificioOficinas(30, 10),
                 new EdificioOficinas(150, 30)
         };
 
-        for (Polideportivo polideportivo : polideportivos) {
+        for (InstalacionDeportiva polideportivo : polideportivos) {
             System.out.println(polideportivo);
         }
         System.out.println("\n");
-        for (EdificioOficinas oficina: edificioOficinas) {
+        for (Edificio oficina : edificioOficinas) {
             System.out.println(oficina);
         }
 
